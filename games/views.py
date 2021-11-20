962446743c5a0e48cf4d7bba5b6a5340c1ff373e
Phurinat Sumateyajarn <6110613194@student.tu.ac.th>
2021-11-20 20:03:54 +0700

    Default Setting

--- games/views.py
+++ games/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def index(request):
+    return render(request,'games/index.html')
+
+def play(request):
+    return render(request,'games/play.html')
+
+def score(request):
+    return render(request,'games/score.html')
--- musicgame/settings.py
+++ musicgame/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-iizim&4iuew7@#7(@ee&8qv!ez&62hcfxhgl+5d58nf$_jji1d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'games',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 # Default primary key field type
