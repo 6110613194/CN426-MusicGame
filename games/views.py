7b37ac64233f24561ee913c6e6b29bb3ef3c17fc
eknarin lerdnuntawat <6110490023@student.tu.ac.th>
2021-11-22 00:47:57 +0700

    version 1.1

--- games/views.py
+++ games/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render,redirect
 from django.urls import reverse
 import requests
-import json
+import json ,random
 
 # Create your views here.
 def index(request):
-    json_data = getPlayList("1eQTM2gSBc9ARw1Pst4V72")
-    return render(request,'games/index.html',{"listData":json_data} )
+    return render(request,'games/index.html')
 
 def play(request):
-    return render(request,'games/play.html')
+    listMusic,listNameMusic = getPlayList("1eQTM2gSBc9ARw1Pst4V72")
+    for i in range(len(listMusic)):
+        randomList = listNameMusic.copy()
+        random.shuffle(randomList)
+        randomList.remove(listMusic[i]["nameMusic"])
+        listMusic[i]["choice"] = [listMusic[i]["nameMusic"],randomList[0],randomList[1]]
+        random.shuffle(listMusic[i]["choice"])
+    return render(request,'games/play.html',{"listMusic":listMusic,"numberMusic":len(listMusic)})
 
 def score(request):
     return render(request,'games/score.html')
@@ -49,12 +55,15 @@ def getPlayList(SID):
     # actual GET request with proper header
     r = requests.get(BASE_URL + OPTION_URL + SID , headers=headers)
 
-    
+    listNameMusic = [] 
     json_data = []
     for i in range(len(r.json()["tracks"]["items"])):
-        json_data.append({"preview_url" : r.json()["tracks"]["items"][i]["track"]["preview_url"],
-                 "nameMusic":  r.json()["tracks"]["items"][i]["track"]["name"] ,
-                 "img" : r.json()["tracks"]["items"][i]["track"]["album"]["images"][0]["url"]})
-    
-    print(json.dumps(json_data, indent=4))
-    return json_data
+        json_data.append({  "preview_url" : r.json()["tracks"]["items"][i]["track"]["preview_url"],
+                            "nameMusic":  r.json()["tracks"]["items"][i]["track"]["name"] ,
+                            "img" : r.json()["tracks"]["items"][i]["track"]["album"]["images"][0]["url"],
+                            "index":i+1
+                            })
+        listNameMusic.append(r.json()["tracks"]["items"][i]["track"]["name"])
+
+    return json_data, listNameMusic
+
